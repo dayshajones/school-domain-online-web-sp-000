532d13535a8ed4e934dd2243ff827992b15cf669
Daysha Jones <daysha.vu@icloud.com>
2021-03-15 23:26:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,10 @@
 class School
-  
-  attr_accessor :name, :roster
-  
-  def initialize(name)
-    @name = name
-    roster = {}
-  end
+
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
 
 end
